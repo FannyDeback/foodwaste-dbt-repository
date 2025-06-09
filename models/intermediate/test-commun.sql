0fa90e5ee1a2648727f2ee5e5ddc7dcee5527922
Sylvia Mandzaka <mandzaka.sylvia@hotmail.fr>
2025-06-09 16:01:07 +0000

    commit

---
+++ models/intermediate/test-commun.sql
@@ -0,0 +1,17 @@
+SELECT 
+DISTINCT Food_product, 
+total_emissions,
+FROM {{ ref('stg_raw__CO2_emissions')}}
+WHERE Food_product IN (SELECT Item FROM {{ ref('stg_raw__Food_productions')}})
+ORDER BY Food_product
+
+SELECT 
+Food_product, 
+total_emissions,
+category
+FROM {{ ref('stg_raw__CO2_emissions')}}
+ORDER BY total_emissions DESC
+
+SELECT 
+country
+FROM {{ ref('stg_raw__Food_Waste_table')}}
