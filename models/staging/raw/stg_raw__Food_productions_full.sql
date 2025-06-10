7aa6880522cd8d1db654528c6e745b405876d16f
Dominique Sylvain <ouechdom@hotmail.fr>
2025-06-10 09:09:59 +0000

    Add new production database with more lines

--- models/staging/raw/stg_raw__Food_productions_full.sql
+++ models/staging/raw/stg_raw__Food_productions_full.sql
@@ -2,27 +2,27 @@ with
 
 source as (
 
-    select * from {{ source('raw', 'Food_productions') }}
+    select * from {{ source('raw', 'Food_productions_full') }}
 
 ),
 
 renamed as (
 
     select
-        domain_code,
+        domain code,
         domain,
-        area_code__m49_,
+        area code _m49_,
         area,
-        element_code,
+        element code,
         element,
-        item_code__cpc_,
+        item code _cpc_,
         item,
-        year_code,
+        year code,
         year,
         unit,
         value,
         flag,
-        flag_description,
+        flag description,
         note
 
     from source
