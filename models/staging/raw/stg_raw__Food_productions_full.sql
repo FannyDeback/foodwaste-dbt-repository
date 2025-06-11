920522c0a6a774ebd5bd5e7fca15bd568e6e00cb
Sylvia Mandzaka <mandzaka.sylvia@hotmail.fr>
2025-06-11 12:12:20 +0000

    commit

--- models/staging/raw/stg_raw__Food_productions_full.sql
+++ models/staging/raw/stg_raw__Food_productions_full.sql
@@ -10,17 +10,10 @@ renamed as (
 
     select
         domain,
-<<<<<<< HEAD
-        Area Code _M49_,
-        area,
-        element,
-        item code _cpc_,
-=======
         `area code _m49_` as area_code,
         area,
         element,
         REGEXP_REPLACE(`item code _cpc_`, r'^0+', '') as item_code,
->>>>>>> 893a9a5331799f9e48c0d0d7b9db2c47da109c33
         item,
         PARSE_DATE('%Y', CAST(year AS STRING)) AS year,
         unit,
